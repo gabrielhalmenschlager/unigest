ecd43fde54a5d9cf4450edd2f5def33670fccbbc
gabriel <gabrielh2007.scs@gmail.com>
2025-08-05 21:07:13 -0300

    registre os apps no arquivo unigest/settings.py

--- unigest/settings.py
+++ unigest/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios',
+    'biblioteca',
+    'cursos',
+    'estoque',
+    'tarefas',
 ]
 
 MIDDLEWARE = [
